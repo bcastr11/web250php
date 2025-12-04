bef2899b867a3f444ec9c5f312e1d0a6913b3883
bcastr11 <158986013+bcastr11@users.noreply.github.com>
2025-12-04 18:44:46 -0500

    commit

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <h2>Home</h2>
         <p>
             Welcome to my WEB250 Webpage. I'll try updating it as the class goes on. I'm not the greatest at being wordy but i'll try. 
-            I do appreciate any feedback I get, and I will do my best to improve from any feedback. Something that I like doing is reading manga. You can learn more about me <a href="introduction.html">here</a>.
+            I do appreciate any feedback I get, and I will do my best to improve from any feedback. Something that I like doing is reading manga. You can learn more about me <a href="introduction.php">here</a>.
         </p>
 
     </main>
