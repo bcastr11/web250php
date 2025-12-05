65143f7608450043355a836e791dd539319b591e
bcastr11 <158986013+bcastr11@users.noreply.github.com>
2025-12-04 23:08:44 -0500

    commmit

--- components/header.php
+++ components/header.php
@@ -5,6 +5,14 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="contract.php">Contract</a></li>
             <li><a href="introduction.php">Introduction</a></li>
+            
+            <li class="dropdown">
+            <a href="#" class="dropbtn">MultiPage</a>
+            <div class="dropdown-content">
+                <a href="superduper_static/index.htm">MultiPage Static</a>
+                <a href="superduper_php/index.php">MultiPage PHP</a>
+            </div>
+            </li>
         </ul>
     </nav>
 </header>
