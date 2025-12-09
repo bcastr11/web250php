a0c02751c0bcbcfe15963218ce871ab028b19203
bcastr11 <158986013+bcastr11@users.noreply.github.com>
2025-12-09 18:54:57 -0500

    cmmmit

--- joyofphp/joyphp/src/db.php
+++ joyofphp/joyphp/src/db.php
@@ -1,12 +1,11 @@
-<?php
-// SQLite version for Wasmer
-$db = new SQLite3('cars.db');
-
-// Create table if not exists
-$db->exec("CREATE TABLE IF NOT EXISTS inventory (
-    VIN TEXT PRIMARY KEY,
-    Make TEXT,
-    Model TEXT,
-    ASKING_PRICE REAL
-)");
+ <?php
+$mysqli = new mysqli('gateway01.us-west-2.prod.aws.tidbcloud.com', 'root', 'hLLlzOJ19LardaLu', 'test','4000');
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+//select a database to work with
+$mysqli->select_db("Cars");
+ 
 ?>
