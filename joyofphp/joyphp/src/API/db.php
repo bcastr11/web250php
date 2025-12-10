15ae03b99a3da5144ee9e3318d3f8da173d59db7
bcastr11 <158986013+bcastr11@users.noreply.github.com>
2025-12-09 19:02:53 -0500

    commmti

--- joyofphp/joyphp/src/API/db.php
+++ joyofphp/joyphp/src/API/db.php
@@ -1,5 +1,14 @@
  <?php
-$mysqli = new mysqli('mySQL', 'root', 'verysecret', 'Cars' );
+$host = 'gateway01.us-west-2.prod.aws.tidbcloud.com';
+$user = 'root';
+$pass = 'hLLlzOJ19LardaLu';
+$db = 'test';
+$port = 4000;
+
+// Enable SSL for TiDB
+$mysqli = mysqli_init();
+$mysqli->ssl_set(NULL, NULL, NULL, NULL, NULL);
+$mysqli->real_connect($host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
